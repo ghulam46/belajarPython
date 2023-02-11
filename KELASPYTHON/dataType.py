b94ba416bab64c8a47cf703557a347ac4d2d09fd
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-11 21:59:45 +0700

    dataType

---
+++ KELASPYTHON/dataType.py
@@ -0,0 +1,20 @@
+# String / text
+teks = "Halo"
+print(teks, "Merupakan tipe data: ", type(teks))
+# print(type(teks))
+
+# Integer / angka
+angka = 1
+print(angka, "Merupakan tipe data: ", type(angka))
+
+# Float / angka dengan koma
+angka2 = 30.5
+print(angka2, "Merupakan tipe data: ", type(angka2))
+
+# Boolean
+bool = True
+print(bool, "Merupakan tipe data: ", type(bool))
+
+# list
+list = [1,2,3]
+print(list, "Merupakan tipe data: ", type(list))
